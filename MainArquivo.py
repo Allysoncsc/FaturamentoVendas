3ba0e61f98f81f45efa72811f386b87c3252a2ab
AllysonCS <allysoncsc@gmail.com>
2023-04-27 18:15:27 -0300

    importando arquivo xlsx

---
+++ MainArquivo.py
@@ -0,0 +1,28 @@
+
+# importar a base de dados
+# usar a biblioteca pandas, vai permiir interagir 
+#com o excel de forma mais simples
+# tive que usar pip install pandas e pip install openpyxl
+import pandas as pd
+#visualizar a base de dados
+
+tabela_vendas = pd.read_excel('Vendas.xlsx')
+
+
+#fatturamento por loja
+
+
+#quantidade de produtos vendidos por loja
+
+
+# tickt medio por produto em cada loja
+
+
+#enviar um email com relaório
+
+
+
+
+
+
+
